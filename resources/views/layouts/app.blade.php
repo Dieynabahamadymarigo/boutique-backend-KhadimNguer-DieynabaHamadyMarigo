1b6378de989e0408c94ec812f98e2c5e042caa1c
Dieynaba <147612958+Dieynabahamadymarigo@users.noreply.github.com>
2026-07-15 01:36:02 +0000

    creation de nav,layouts/app et home  blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title> @yield('title', 'Boutique') </title>
+
+        <link href="{{ asset('css/site.css') }}" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    </head>
+
+    <body>
+
+        {{-- le menu --}}
+        <header>
+            @include('partials.navigation')
+        </header>
+
+        {{-- le contenu de la page --}}
+        <main>
+            {{-- Le message de succès de -> with --}}
+            @if(session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+
+            {{-- le contenu principal de chaque page --}}
+            @yield('content')
+
+        </main>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </body>
+</html>
